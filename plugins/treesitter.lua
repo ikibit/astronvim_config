1872054f17e884b7b6b5d260fd0a2208343ee0f2
Barbaros <77664141+ikibit@users.noreply.github.com>
2023-04-23 15:48:15 +0300

    Update treesitter.lua

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { 'html', 'css', 'javascript', 'typescript', 'tsx', 'json', 'go', 'lua', 'python', 'help', 'vim',
+    ensure_installed = { 'html', 'css', 'javascript', 'typescript', 'tsx', 'json', 'go', 'lua', 'python', 'vim',
       'elixir',
       'heex', 'eex', 'c_sharp' },
   },
